0f41de64ada461a38ac1f448d34597e6ed0b7112
Ramesh Prajapati <ramesh.prajapathi@asiglobal.co>
2019-01-27 19:05:00 +0530

    Created ‘Header

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React  from 'react';
+
+// Functional Component
+const Header = function(){
+    return(
+        <div className="header">
+          Phone Directory
+         </div>
+    )
+}
+
+export default Header;
