afd24f6a5cb2449c15f80257f5467ff749811b4f
Ramesh Prajapati <ramesh.prajapathi@asiglobal.co>
2019-01-27 19:51:31 +0530

    Styled ‘Header’ (Inline Styling)

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,16 @@ import React  from 'react';
 
 // Functional Component
 const Header = function(){
+    const headerStyle = {
+        textAlign: 'center' , 
+        padding: 20,
+        background: '#000' ,
+        color: '#fff', 
+        textTransform:'uppercase'
+    }
+    
     return(
-        <div className="header">
+        <div style={headerStyle}>
           Phone Directory
          </div>
     )
