2e5c44a11af4145ba97f87485d6c2106610bdb27
Ramesh Prajapati <ramesh.prajapathi@asiglobal.co>
2019-02-06 00:38:04 +0530

    Added ‘AddSubscriber’ component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css'
 
 class App extends Component {
 
+  clickHandler(message){
+    alert(message)
+  }
+
   render() {
     let subscribers = [
       {
@@ -33,7 +37,7 @@ class App extends Component {
                  <span className="grid-item">{sub.name}</span>
                   <span className="grid-item">{sub.phone}</span>
                   <span className="grid-item action-btn-container">
-                  <button className="custom-btn delete-btn">Delete</button>
+                  <button className="custom-btn delete-btn" onClick={this.clickHandler.bind(this, "Delete Clicked")}>Delete</button>
                   </span>
                 </div>
             })
